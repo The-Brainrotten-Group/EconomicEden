6e3d1ee582dc40579b456957a5bb21c2125eddc0
Elvin Bajric <elvinbaj@proton.me>
2024-05-19 22:02:29 +0200

    Small changes from template
    Changed DB name, changed ports and update docker-compose.yml

--- api/src/main.rs
+++ api/src/main.rs
@@ -45,7 +45,7 @@ async fn main() {
     };
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+        .allow_origin("http://localhost:6902".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
@@ -57,6 +57,6 @@ async fn main() {
     .layer(cors);
 
     println!("🚀 Server started successfully");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:6901").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
